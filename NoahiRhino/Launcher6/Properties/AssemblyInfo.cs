aa34cec0f776bd98470f4fd70d156780519690ae
KaivnD <KaivnD@hotmail.com>
2020-05-22 16:41:34 +0800

    feat:通过Launcher6在一个Rhino实例中执行python脚本了

--- NoahiRhino/Launcher6/Properties/AssemblyInfo.cs
+++ NoahiRhino/Launcher6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NoahInRhino")]
+[assembly: AssemblyTitle("Launcher6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NoahInRhino")]
+[assembly: AssemblyProduct("Launcher6")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8e90f0f5-58b3-4ed0-a233-a7e86f2b9e24")]
+[assembly: Guid("9abb518b-1474-48e5-8734-0673aa65a1b9")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
