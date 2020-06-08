238918788cc18ae57533bc2fee0cc3fb4375448e
KaivnD <KaivnD@Hotmail.com>
2020-06-08 23:26:07 +0800

    add:增加ws Client

---
+++ NoahiRhino/NoahiRhino/NoahClient.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Eto.Forms;
+using Rhino;
+using Rhino.UI;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Eto.Drawing;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Grasshopper.GUI.Canvas;
+using Grasshopper;
+using Rhino.Input;
+
+namespace NoahiRhino
+{
+    public class NoahClient
+    {
+        internal int Port;
+        internal string WorkDir;
+        internal Guid Guid;
+        private WebSocket Client;
+
+        private int RetryCnt = 0;
+        private int MaxRetry = 5;
+
+        public NoahClient(int port, string workDir)
+        {
+            WorkDir = workDir;
+            Port = port;
+            Guid = Guid.NewGuid();
+
+            Init();
+        }
+
+        public void Connect()
+        {
+            Client.Connect();
+
+            Client.Send("{\"route\": \"none\", \"msg\": \"This is Rhino\"}");
+        }
+
+        public void Close()
+        {
+            Client.Close();
+        }
+
+        private void Init()
+        {
+            Client = new WebSocket("ws://localhost:" + Port.ToString() + "/data/server/?platform=Rhino&ID=" + Guid.ToString());
+
+            Client.OnMessage += Socket_OnMessage;
+            Client.OnError += Socket_OnError;
+            Client.OnOpen += Socket_OnOpen;
+            Client.OnClose += Socket_OnClose;
+        }
+
+        private void Socket_OnClose(object sender, CloseEventArgs e)
+        {
+            RhinoApp.WriteLine("Noah Client connecting is closed");
+        }
+
+        public async void Reconnect()
+        {
+            while (RetryCnt < MaxRetry)
+            {
+                ++RetryCnt;
+
+                RhinoApp.WriteLine("Retrying to connect Noah Client " + RetryCnt + " times.");
+                Connect();
+
+                if (Client.ReadyState == WebSocketState.Open) return;
+
+                await Task.Delay(100);
+            }
+        }
+
+        private void Socket_OnOpen(object sender, EventArgs e)
+        {
+            RetryCnt = 0;
+        }
+
+        private void Socket_OnError(object sender, ErrorEventArgs e)
+        {
+            RhinoApp.WriteLine(e.Message);
+        }
+
+        private void Socket_OnMessage(object sender, MessageEventArgs e)
+        {
+            try
+            {
+                RhinoApp.WriteLine(e.Data);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public class ClientEventArgs
+    {
+        public ClientEventType route;
+        public string data;
+    }
+
+    public enum ClientEventType
+    {
+        message,
+        task,
+        data,
+        group,
+        pick
+    }
+}
