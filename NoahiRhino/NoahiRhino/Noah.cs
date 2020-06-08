481a28d8a766ef5c39c94071832f94b3ffd4e06b
KaivnD <KaivnD@hotmail.com>
2020-06-08 16:48:40 +0800

    add:GH相关操作

--- NoahiRhino/NoahiRhino/Noah.cs
+++ NoahiRhino/NoahiRhino/Noah.cs
@@ -1,28 +1,153 @@
-﻿using Grasshopper.Plugin;
+﻿using Grasshopper;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Special;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Plugin;
 using RestSharp;
 using Rhino;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NoahiRhino
 {
-    public class Noah
+    public static class Noah
     {
-        public Noah()
+        public static void LaunchGrasshopper()
         {
-            void LaunchGh()
+            if (!(RhinoApp.GetPlugInObject("Grasshopper") is GH_RhinoScriptInterface gh)) return;
+            
+            if (gh.IsEditorLoaded()) return;
+
+            gh.DisableBanner();
+            gh.LoadEditor();
+
+            Instances.AutoHideBanner = false;
+            Instances.AutoShowBanner = false;
+            Instances.ComponentServer.DestroyLoadingUI();
+        }
+
+        public static void LoadDoc(string file)
+        {
+            var io = new GH_DocumentIO();
+            if (!io.Open(file)) throw new Exception("读取文档失败");
+
+            GH_DocumentServer server = Instances.DocumentServer;
+
+            var doc = io.Document;
+
+            if (server == null) throw new Exception("读取文档失败");
+
+            server.AddDocument(doc);
+
+            GH_Canvas activeCanvas = Instances.ActiveCanvas;
+            if (activeCanvas == null) throw new Exception("读取文档失败");
+
+            activeCanvas.Document = doc;
+            activeCanvas.Document.IsModified = false;
+            activeCanvas.Refresh();
+        }
+
+        public static void AssignDataToDoc(Dictionary<string, object> dataSet)
+        {
+            GH_Canvas activeCanvas = Instances.ActiveCanvas;
+            if (activeCanvas == null) throw new Exception("读取文档失败");
+
+            GH_Document doc = activeCanvas.Document;
+
+            if (doc == null) return;
+
+            var hooks = doc.ClusterInputHooks();
+
+            foreach (var hook in hooks)
             {
-                if (!(RhinoApp.GetPlugInObject("Grasshopper") is GH_RhinoScriptInterface gh)) return;
+                GH_Structure<IGH_Goo> m_data;
 
-                gh.DisableBanner();
+                string key = hook.NickName;
 
-                if (!gh.IsEditorLoaded())
+                if (string.IsNullOrEmpty(key)) key = hook.Name;
+                if (string.IsNullOrEmpty(key)) key = hook.CustomName;
+                if (string.IsNullOrEmpty(key)) key = hook.CustomNickName;
+
+                if (!dataSet.TryGetValue(hook.NickName, out object data)) continue;
+
+                m_data = SingleDataStructrue(data);
+
+                hook.ClearPlaceholderData();
+                hook.SetPlaceholderData(m_data);
+                hook.ExpireSolution(true);
+            }
+
+            // for data placeholder inside cluster (deep = 1)
+
+            var clusters = new List<GH_Cluster>();
+            foreach (var obj in doc.Objects)
+            {
+                if (!(obj is GH_Cluster cluster)) continue;
+                clusters.Add(cluster);
+            }
+
+            if (clusters.Count == 0) return;
+
+
+            foreach (var cluster in clusters)
+            {
+                foreach (var obj in cluster.Document("").Objects)
                 {
-                    gh.LoadEditor();
+                    if (!(obj is IGH_Param param)) continue;
+
+                    string nickname = param.NickName;
+
+                    if (string.IsNullOrEmpty(nickname)) nickname = param.Name;
+                    if (!nickname.StartsWith("@", StringComparison.Ordinal)) continue;
+
+                    if (!dataSet.TryGetValue(nickname, out object data)) continue;
+                                       
+
+                    Utility.InvokeMethod(param, "Script_ClearPersistentData");
+                    Utility.InvokeMethod(param, "Script_AddPersistentData", new List<object>() { data });
+
+                    param.ExpireSolution(true);
+                    cluster.ExpireSolution(true);
                 }
+            }
+
+            doc.NewSolution(true);
+
+            activeCanvas.Document.IsModified = false;
+            activeCanvas.Refresh();
+
+            GH_Structure<IGH_Goo> SingleDataStructrue(object value)
+            {
+
+                GH_Structure<IGH_Goo> m_data = new GH_Structure<IGH_Goo>();
+
+                GH_Number castNumber = null;
+                GH_String castString = null;
+                GH_Curve castCurve = null;
+                if (GH_Convert.ToGHCurve(value, GH_Conversion.Both, ref castCurve))
+                {
+                    m_data.Append(new GH_ObjectWrapper(castCurve));
+                }
+                else if (GH_Convert.ToGHNumber(value, GH_Conversion.Both, ref castNumber))
+                {
+                    m_data.Append(new GH_ObjectWrapper(castNumber));
+                }
+                else if (GH_Convert.ToGHString(value, GH_Conversion.Both, ref castString))
+                {
+                    m_data.Append(new GH_ObjectWrapper(castString));
+                }
+                else
+                {
+                    m_data.Append((IGH_Goo)value);
+                }
+
+                return m_data;
             }
         }
     }
